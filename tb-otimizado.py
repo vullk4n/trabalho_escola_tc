1d1d645ff856dc152e6a59a320e977d9f6dddd81
vulkan-ops <vulkanops@gmail.com>
2022-03-19 00:50:24 -0300

    declare answers!
    Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- tb-otimizado.py
+++ tb-otimizado.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python
+def print_answers(answers):
+    print(f"Respostas obtidas:    {answers}\n",
+          f"    Menor valor:      {min(answers)}\n",
+          f"    Maior valor:      {max(answers)}\n",
+          f"    Média Aritmética: {sum(answers) / len(answers)}\n");
