dc3866edb0c5e6fb19141b8207d265847f094be3
vulkan-ops <vulkanops@gmail.com>
2022-03-19 00:36:34 -0300

    add first question and its values
    Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- trabalho-escola-tecnica.py
+++ trabalho-escola-tecnica.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
+
 print("Equipe Segurança")
+
+print("\nVocê se sente segura quando sai e quando esta em casa?")
+SE=[1,2,6,8,1,2,8,2,8,1]
+print("\nResposta obtidas: ",SE)
+print("\nMenor valor:", min(SE) )
+print("Maior valor:" , max(SE) )
+print("media valores:", sum(SE)/len(SE) )
 
